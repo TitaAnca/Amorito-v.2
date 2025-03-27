08439c130e48cae0ad49c84d273cd117c94e5bbc
andy <andy2valtoa@gmail.com>
2025-03-27 12:26:49 +0100

    rutas

--- app/rutas/matchRUTA.py
+++ app/rutas/matchRUTA.py
@@ -3,7 +3,6 @@ from app.controllers.matchControl import crear_match_controlador
 
 match_bp = Blueprint('match', __name__, url_prefix='/match')
 
-# Ruta para crear una nueva coincidencia
 @match_bp.route('/crear_match', methods=['POST'])
 def crear_match():
     return crear_match_controlador()
