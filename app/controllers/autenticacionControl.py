68d79c7bf98a9a7fc2c4d398ec37d5bc532e3645
andy <andy2valtoa@gmail.com>
2025-03-28 08:37:57 +0100

    agregar a BBDD

--- app/controllers/autenticacionControl.py
+++ app/controllers/autenticacionControl.py
@@ -1,26 +1,33 @@
 from flask import request, jsonify
 from app.servicios.usuarioServicio import registrar_usuario, iniciar_sesion
 
-
 # Ruta de registro
 def registrar_usuario_controlador():
     datos = request.get_json()
 
     # Validar la entrada
-    if not datos.get("nombre_usuario") or not datos.get("contrasena") or not datos.get("edad") or not datos.get("genero") or not datos.get("id_usuario"):
-        return jsonify({"mensaje": "El nombre de usuario, la contraseña, la edad y el género son obligatorios"}), 400
+    if not datos.get("nombre_usuario") or not datos.get("contrasena") or not datos.get("edad") or not datos.get("genero") or not datos.get("id_usuario") or not datos.get("orientacion_sexual"):
+        return jsonify({"mensaje": "El nombre de usuario, la contraseña, la edad, el género y la orientación sexual son obligatorios"}), 400
     
     id_usuario = datos.get("id_usuario")
     nombre_usuario = datos.get("nombre_usuario")
     contrasena = datos.get("contrasena")
-    edad = datos.get("edad")
+    try:
+        edad = int(datos.get("edad"))
+    except ValueError:
+        return jsonify({"mensaje": "La edad debe ser un número válido"}), 400
     genero = datos.get("genero")
+    orientacion_sexual = datos.get("orientacion_sexual")
+
+    # Validar que la orientación sexual esté en los valores permitidos
+    if orientacion_sexual not in ["heterosexual", "bisexual", "homosexual", "pansexual"]:
+        return jsonify({"mensaje": "La orientación sexual no es válida"}), 400
 
     # Registrar al usuario
-    if registrar_usuario(id_usuario , nombre_usuario, contrasena, edad, genero):
+    if registrar_usuario(id_usuario, nombre_usuario, contrasena, edad, genero, orientacion_sexual):
         return jsonify({"mensaje": "Usuario registrado correctamente"}), 201
     else:
-        return jsonify({"mensaje": "El usuario ya existe o el género no es válido"}), 400
+        return jsonify({"mensaje": "El usuario ya existe o los datos no son válidos"}), 400
 
 # Ruta de inicio de sesión
 def iniciar_sesion_controlador():
