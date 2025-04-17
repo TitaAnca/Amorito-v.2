3c1bb32192ca86dddce8488eb11c88bdeb39a0aa
andy <andy2valtoa@gmail.com>
2025-04-17 19:25:36 +0200

    matchControl

--- app/controllers/matchControl.py
+++ app/controllers/matchControl.py
@@ -8,9 +8,12 @@ def crear_match_controlador():
     if not id_usuario1 or not id_usuario2:
         return jsonify({"error": "Ambos ID de usuario son requeridos"}), 400
 
+    if id_usuario1 == id_usuario2:
+        return jsonify({"error": "Un usuario no puede hacer match consigo mismo"}), 400
+    
     match = crear_match(id_usuario1, id_usuario2)
 
     if match:
-        return jsonify({"mensaje": "Match creada exitosamente", "match": match.__dict__}), 201
+        return jsonify({"mensaje": "Match creada exitosamente", "match": match.to_dict()}), 201
     else:
         return jsonify({"error": "No se encontró un match (verifica edad, preferencias y compatibilidad de género)"}), 400
